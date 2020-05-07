00564731dec437c81651027d1a91422b1dd1d8dc
codefuncode <codefuncode@protonmail.com>
2020-05-07 19:46:10 -0400

    Actualizado algunas varibles extra para el control del flijo de ejecucion

--- Tabla-de-verdad/js/js.js
+++ Tabla-de-verdad/js/js.js
@@ -13,15 +13,21 @@ var conjunto = false;
 var operando = false;
 var boleano = false;
 var ecuacion = [];
+var tocaBoleano = false;
+var cierraParentesis = false;
 
 pantalla.value = texto;
+
 // ===========================================
 
 for (var i = 0; i < boton.length; i++) {
+
     boton[i].addEventListener('click', imprime);
 }
 
+// ============================================
 function imprime(argument) {
+
     if (this.value == 'AC') {
         texto = '';
         conjunto = false;
@@ -31,12 +37,20 @@ function imprime(argument) {
         creafuncion(this.value);
     }
 }
-//  Dentro de esta función  se pretende generar una ecuación matemática  que pueda decir el valor de la verdad de  una formula 
+// ============================================
+
+// ========================================================
+//  Dentro de esta función  se pretende generar una ecuación matemática  
+// que pueda decir el valor de la verdad de una formula 
+// ========================================================
+
 function creafuncion(valor) {
     dato = valor;
+
     // ======================================================
     //  Mitras en valor del paréntesis  sea el del indice  del primer paréntesis 
     // y no se ha creado el paréntesis  no se podrá escribir el mismo paréntesis de inicio 
+
     while (valor == paraentesis[0] && !(conjunto)) {
 
         texto = texto += dato;
@@ -44,8 +58,9 @@ function creafuncion(valor) {
         conjunto = true;
     }
     // ======================================================
+
     // ======================================================
-    //  Solo permite escibirel parentesis de cierre si se ha abierto uno previamente 
+    //  Solo permite escribir el paréntesis de cierre si se ha abierto uno previamente 
     while (valor == paraentesis[1] && conjunto) {
 
         texto = texto += dato;
@@ -54,14 +69,25 @@ function creafuncion(valor) {
     }
     // ======================================================
 
-    // =====================================================
+    // =========================================================================
+    // Si se ha escrito  un operador  pues se necesitara un valor 
+    // boleano es qui de ne se toma esa decisión 
     while (!(operando) && (verdadero.includes(dato) || falso.includes(dato))) {
 
         texto = texto += dato;
         pantalla.value = texto;
         operando = true;
     }
+    // =========================================================================
+
     // =====================================================
+    // Si ya se escribió un operador y el valor que viene del datos 
+    // esta incluido en los valores de operadores  entonces se hará esto,
+    // sin embargo no me queda clara esta  condición por que me parece 
+    // que necesito mas condiciones para que esto se realice Para esto he 
+    // declarado otras variables para definir el tipo de elemento que se 
+    // aceptara después  de  haber escrito un operador  de hecho después 
+    // del operador toca un valor boleano  y después toca cerrar  o abrir paréntesis 
     if (operando && operador.includes(dato)) {
 
         texto = texto += dato;
@@ -93,6 +119,9 @@ function creafuncion(valor) {
     //     alert('Presionaste una variable verdadera  y haz colocado  parentesis ');
     // }
 }
+
+// =================================================
+
 // ===========================================
 // ========================================
 //  ver botones seleccionados en consola 
