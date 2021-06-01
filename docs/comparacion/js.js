0e63c9d8d4fc891b664b0f36aa40bcf8afb6fc47
Carlos Aleman <alemandiazcarlos@protonmail.com>
2021-05-31 23:44:56 -0400

    Carpeta comparacion de otro ejemplo que no es del enunciado del banco

---
+++ docs/comparacion/js.js
@@ -0,0 +1,45 @@
+let _persona = [{
+    nombre: "",
+    género: "",
+    edad: "",
+    oficio: "",
+    altura: "",
+    contextura: "",
+    tipo_cuerpo: "",
+    color_de_piel: "",
+    color_de_cabello: "",
+    tipo_de_cabello: "",
+    largo_del_cabello: "",
+    personalidad: "",
+    vestimenta: "",
+    forma_del_rostro: "",
+    grosor_de_labios: "",
+    forma_de_la_nariz: "",
+    forma_de_los_ojos: "",
+    color_de_ojos: "",
+    actividades_de_ocio: "",
+    actividades_caseras: "",
+    actividades_externas: "",
+    genero_musical: "";
+}];
+
+let nombre = [];
+let género = [];
+let edad = [];
+let oficio = [];
+let altura = [];
+let contextura = [];
+let color_de_piel = [];
+let color_de_cabello = [];
+let tipo_de_cabello = [];
+let largo_del_cabello = [];
+let vestimenta = [];
+let forma_del_rostro = [];
+let grosor_de_labios = [];
+let forma_de_la_nariz = [];
+let forma_de_los_ojos = [];
+let color_de_ojos = [];
+let actividades_de_ocio = [];
+let actividades_caseras = [];
+let actividades_externas = [];
+let genero_musical = [];
